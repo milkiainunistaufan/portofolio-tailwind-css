5f4b0769ce5ac00793f9522a20b5233bc6816232
unknown <milkiainun@gmail.com>
2023-04-03 13:42:56 +0700

    menambah maps

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,17 +19,3 @@ hamburger.addEventListener('click', function() {
     navMenu.classList.toggle('hidden');
 });
 
-function initMap() {
-    // The location of Uluru
-const uluru = { lat: -8.198148, lng: 112.507982 };
-// The map, centered at Uluru
-const map = new google.maps.Map(document.getElementById("map"), {
-  zoom: 4,
-  center: uluru,
-});
-
-const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
-}
